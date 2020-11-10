11c0ff91b0d7d0867103fc42748762924bcc3803
RickGVonk <302162390@student.rocmondriaan.nl>
2020-11-10 14:27:24 +0100

    update health.php

---
+++ health.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <title>HealthOne</title>
+</head>
+<body>
+<header>
+
+</header>
+<main>
+
+</main>
+<nav>
+
+</nav>
+<footer>
+
+</footer>
+</body>
+</html>
