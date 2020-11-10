bca59ff9e5db4c197e9b46c91db5c8e9aa5a2c54
RickGVonk <302162390@student.rocmondriaan.nl>
2020-11-10 15:01:52 +0100

    login begonnen

--- health.php
+++ health.php
@@ -10,7 +10,19 @@
 
 </header>
 <main>
-
+   <section class="login">
+       <label for="username">Gebruikersnaam:</label><br/>
+       <input type="text" id="username" name="username" placeholder="Gebruikersnaam"><br/><br/>
+       <label for="password">Wachtwoord:</label><br/>
+       <input type="text" id="password" name="password" placeholder="Wachtwoord"><br/><br/>
+       <input type="button" id="submit" value="Login">
+   </section>
+<!--    Welkom in de zilver kruis webapp-->
+<!--    hier kunt u de volgende informatie vinden en toevoegen-->
+<!--    insert info waar ze toegang tot hebben-->
+<!--    <div>-->
+<!--        -->
+<!--    </div>-->
 </main>
 <nav>
 
